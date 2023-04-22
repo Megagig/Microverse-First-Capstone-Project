285f917c19e861dce694dbea9f0a720731db6772
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-04-22 20:59:37 +0100

    Create Featured speaker section Dynamically

--- scripts.js
+++ scripts.js
@@ -22,3 +22,77 @@ function closeMenu() {
 
 // Select all the nav links and add a click event to each one that triggers the closeMenu function
 navLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+// CREATING FEATURED SPEAKER SECTION DYNAMICALLY
+const speakers = document.querySelector('.features-speakers');
+
+const speakersDetails = [
+  {
+    id: 1,
+    name: 'Pharm. Folashade Lawal',
+    title: 'CEO, Victory Drugs Limited.',
+    speakerImg: 'images/Folashade-lawal.png',
+    about:
+      'Pharm. Folash Lawal is the CEO of Victory Limited. A leading community Pharmacy in FESTAC, Lagos',
+  },
+  {
+    id: 2,
+    name: 'Dr. Peter Bamkole',
+    title: 'Physician, Hilton-Top Hospital Ikoyi, Lagos.',
+    speakerImg: 'images/Dr-Peter-Bamkole.jpg',
+    about:
+      'Peter Bamkole, pioneered the Enterprise Development Centre (EDC) of the Pan-Atlantic University in January 2003, now one of the top enterprise development centers in Africa.',
+  },
+  {
+    id: 3,
+    name: 'Pharm. Bisi Bright',
+    title: 'Consultant Clinical Pharmacist and Public Health Manager.',
+    speakerImg: 'images/Pharm.-Bisi-Bright.jpeg',
+    about:
+      'Pharm. (Dr) Mrs. Bisi Bright is a Consultant Clinical Pharmacist and Public Health Manager. A fellow of both the Pharmaceutical Society of Nigeria and the West African Postgraduate College of Pharmacists (WAPCP), she also holds a Masters’ in Public Health.',
+  },
+  {
+    id: 4,
+    name: 'Chidi Okoro',
+    title: 'Executive Consultant of Drug and Medicines.',
+    speakerImg: 'images/speaker4.jpg',
+    about:
+      'Chidi Okoro teaches Strategy at Lagos Business School. He is currently the Founder/Executive Consultant of Drugs and Medicaments Nigeria Limited, a retail chain/own brands dealer in the Pharmaceutical sector.',
+  },
+  {
+    id: 5,
+    name: 'Abimbola Adebakin',
+    title: 'CEO, Advantage Health Africa',
+    speakerImg: 'images/Abimbola-Adebakin.jpg',
+    about:
+      'Nana Aba Anamoah is a Ghanaian award-winning media personality,news anchor, and broadcaster.',
+  },
+  {
+    id: 6,
+    name: 'Adedotun Sulaiman',
+    title: 'Veteran Sports Journalist',
+    speakerImg: 'images/Adedotun-Sulaiman.jpg',
+    about:
+      ' Mr. Yeboah is a Ghanaian veteran sports journalist, writer and commentator.',
+  },
+];
+
+function randomSpeaker() {
+  speakers.innerHTML = speakersDetails
+    .map(
+      (speaker) => `
+           <div class="speakers-details">
+              <img src=${speaker.speakerImg} alt="UG Vice-chancellor" />
+              <div class="details">
+                <h4>${speaker.name}</h4>
+                <p class="title">${speaker.title}</p>
+                <hr />
+                <p class="position">${speaker.about}</p>
+              </div>
+            </div>
+  `
+    )
+    .join('');
+}
+
+randomSpeaker();
